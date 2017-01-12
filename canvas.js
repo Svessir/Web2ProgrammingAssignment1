dceebdbeaeb6f0feec411b7af6b7a2a6ae3fb334
sverrir <sverrir1993@gmail.com>
2017-01-12 12:52:02 +0000

    bleh

--- canvas.js
+++ canvas.js
@@ -1,5 +1,20 @@
+/**
+ * Canvas class contains the state of the
+ * canvas.
+ */
 function Canvas(width, height, canvasContext) {
     this.width = width;
     this.height = height;
     this.canvasContext = canvasContext;
+    this.canvasObjects = [];
+    this.undoStack = [];
+    this.redoStack = [];
+}
+
+/**
+ * Clears the canvas and draws all objects
+ * in the canvas object list back to the canvas.
+ */
+Canvas.prototype.update = function update() {
+
 }
