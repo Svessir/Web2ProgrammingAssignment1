60704cb042b1e5c2c9f5f4c9a6247c0b3ff49870
sverrir <sverrir1993@gmail.com>
2017-01-12 15:08:05 +0000

    the canvas now responds to mouse events

--- canvas.js
+++ canvas.js
@@ -2,13 +2,15 @@
  * Canvas class contains the state of the
  * canvas.
  */
-function Canvas(width, height, canvasContext) {
-    this.width = width;
-    this.height = height;
-    this.canvasContext = canvasContext;
+function Canvas(canvasElement) {
+    this.canvasElement = canvasElement;
+    this.canvasContext = canvasElement.getContext("2d");;
     this.canvasObjects = [];
     this.undoStack = [];
     this.redoStack = [];
+
+    // Assign tool functions to canvas mouse event functions
+    this.setTool(undefined); // Insert default tool
 }
 
 /**
@@ -17,4 +19,13 @@ function Canvas(width, height, canvasContext) {
  */
 Canvas.prototype.update = function update() {
 
+}
+
+/**
+ * Set a new tool for the canvas
+ */
+Canvas.prototype.setTool = function setTool(tool) {
+    this.canvasElement.onmousedown = function(event) {tool.mouseDown(event)};
+    this.canvasElement.onmousemove  = function(event) {tool.mouseMove(event)};
+    this.canvasElement.onmouseup = function(event) {tool.mouseUp(event)};
 }
--- startApplication.js
+++ startApplication.js
@@ -1,3 +1,7 @@
+var canvas;
+
 window.onload = function () {
     // Add application start up logic here
+    var canvasElement = document.getElementById("canvas");
+    canvas = new Canvas(canvasElement);
 }
